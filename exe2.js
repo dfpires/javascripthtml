3b92bac2cfdfe07ce333405c35cb75cb023e0036
Daniel Facciolo Pires <daniel@unifacef.edu.br>
2019-08-26 20:27:03 -0300

    alterando exe 2

--- exe2.js
+++ exe2.js
@@ -13,14 +13,19 @@ function funcao(){
     var somaSalario = 0
     var somaFilhos = 0
     var maiorSalario = vet[0].salario
+    var qtde = 0;
     for(var i=0;i<vet.length;i++){
         somaSalario = somaSalario + vet[i].salario
         somaFilhos = somaFilhos + vet[i].filhos
         if (vet[i].salario > maiorSalario){
             maiorSalario = vet[i].salario
         }
+        if ((vet[i].sexo == 'F') && (vet[i].salario > 1000)){
+            qtde++;
+        }
     }
     alert("Média salarial " + somaSalario/vet.length)
     alert("Média de filhos " + somaFilhos/vet.length)
     alert("Maior salário " + maiorSalario)
+    alert("Percentual " + qtde/3*100);
 }
